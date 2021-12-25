36e1b533479349c44e2734dfe5ea693eb7ca2c67
glitch-prog <itgeniusofblr@gmail.com>
2021-12-25 17:26:52 +0300

    feat(firebase,firestore,todo):firebase has been connected; test todo has been added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 // import {
 //   createUserWithEmailAndPassword,
@@ -15,15 +15,28 @@ import Login from './components/Login.js';
 import Start from './components/Start';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
-import Form from './components/Form.js';
+// import Form from './components/Form.js';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from './firebase-config';
 
 export default function App() {
+  const [newTest, setNewTest] = useState('');
+  const todosCollectionRef = collection(db, 'todos');
+
+  const createUser = async () => {
+    await addDoc(todosCollectionRef, { test: newTest });
+  };
+
   return (
     <div className='App'>
-      {/* <Link to='/register_page'>Start!</Link> */}
-      {/* <Login />
-      <Logout /> */}
-      <Form />
+      <input
+        placeholder='name'
+        onChange={(event) => {
+          setNewTest(event.target.value);
+        }}
+      ></input>
+      <button onClick={createUser}>add</button>
+
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/login_page' element={<Login />} />
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const database = getDatabase(app);
+export const db = getFirestore(app);
+
 export const auth = getAuth(app);
