48f96d9b7835466846cd7a840323ef2d96fb4a78
glitch-prog <itgeniusofblr@gmail.com>
2022-01-14 20:24:42 +0300

    рр

--- src/components/Calendar/DateButtton.js
+++ src/components/Calendar/DateButtton.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { useState } from 'react/cjs/react.development';
 
 export default function DateButtton() {
-<<<<<<< HEAD
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const [buttons, setButtons] = useState(arr);
   // let day = 0;
-=======
 
   let day = 1;
   function getTodayDay() {
@@ -17,17 +15,15 @@ export default function DateButtton() {
 
   getTodayDay();
 
-  function getDateCal(){
+  function getDateCal() {
     let div = document.querySelector('.root');
     div.addEventListener('click', (event) => {
       if (event.target.className === 'calendarBtn') {
         day = event.target.textContent;
         console.log(day);
       }
-    })}
-    
-  
->>>>>>> eedd8df8f948c94d5ae52a49149f9dbe2e07ebc2
+    });
+  }
 
   return (
     <div>
