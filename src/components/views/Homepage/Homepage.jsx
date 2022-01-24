7d6a588342a9b1565f1edc55bb2b6f6bd4b307d0
glitch-prog <dubovkasfamily@gmail.com>
2022-01-24 23:00:55 +0300

    feat(styles): styles have been added for a Start page and a Homepage

--- src/components/views/Homepage/Homepage.jsx
+++ src/components/views/Homepage/Homepage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { START_PAGE } from '../../../constants/constants';
+import styles from './Homepage.module.css';
 
 export default function Homepage() {
   return (
-    <div>
-      <h3>Hello, this is a Clever To-Do-List</h3>
-      <p>Let's start</p>
-      <Link to={START_PAGE}>Start</Link>
+    <div className={styles.main__block}>
+      <div className={styles.homepage__card}>
+        <h3 className={styles.homepage__card__title}>
+          Hello, this is a Clever To-Do-List
+        </h3>
+        <p className={styles.homepage__card__description}>Let's start</p>
+        <button className={styles.start__btn}>
+          <Link to={START_PAGE} className={styles.link}>
+            Start
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/views/Start/Start.jsx
+++ src/components/views/Start/Start.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { REGISTER_PAGE, LOGIN_PAGE } from '../../../constants/constants';
+import styles from './Start.module.css';
 
 export default function Start() {
   const date = new Date();
   console.log(date);
   return (
-    <div>
-      <Link to={REGISTER_PAGE}>Register</Link>
-      <Link to={LOGIN_PAGE}>Log In</Link>
+    <div className={styles.main__block}>
+      <div className={`${styles.start__card} ${styles.start__card__sign__up}`}>
+        <h3>Sign up</h3>
+        <p>If you have an account, click here</p>
+        <Link to={REGISTER_PAGE}>Sign up</Link>
+      </div>
+
+      <div className={`${styles.start__card} ${styles.start__card__sign__in}`}>
+        <h3>Sign in</h3>
+        <p>If you want to create a new account, click here</p>
+        <Link to={LOGIN_PAGE}>Sign in</Link>
+      </div>
     </div>
   );
 }
